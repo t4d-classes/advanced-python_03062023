aa0c4c2552da89884c451b4bc3aa710bf0f9611f
Eric Greene <eric@t4d.io>
2023-03-09 14:02:52 -0500

    add more async demos

---
+++ exercises/exercise-8.md
@@ -0,0 +1,7 @@
+# Exercise 8
+
+1. Using the function `get_rates_sync` as an example, implement a new version named `get_rates_async` that uses async/await. The new function should receive the same parameters as `get_rates_sync`.
+
+2. Ensure the that REST API has 1 second delay when responding to requests.
+
+3. Add `get_rates_async` to `rates_demo.py`. Compare the time difference between the various approaches.
---
+++ language_demos/async_demos/task_async.py
@@ -0,0 +1,62 @@
+""" single async """
+
+import asyncio
+from random import randint
+
+def delay() -> float:
+    """ delay """
+    return randint(1,10) / 2
+
+
+async def get_data(task_num: int = 0) -> int:
+    """ get data """
+    print(f"start get data: {task_num}")
+    await asyncio.sleep(delay())
+    print(f"end get data: {task_num}")
+    return task_num
+
+
+async def main() -> None:
+    """ main """
+    # coroutine_obj = get_data()
+    # print(type(coroutine_obj))
+
+    # task_obj = asyncio.create_task(coroutine_obj)
+    # await asyncio.sleep(1)
+    # print("task object created")
+    # print(type(task_obj))
+
+    # await task_obj
+
+    def all_done(t):
+        print(f"all done : task {t.result()}")
+
+    task1 = asyncio.create_task(get_data(1))
+    task2 = asyncio.create_task(get_data(2))
+
+    task1.add_done_callback(all_done)
+    task2.add_done_callback(all_done)
+
+    # print("a")
+    # await get_data(1)
+    # print("b")
+
+
+    # def print_b(t):
+    #     print("b") 
+    
+    # print("a")
+    # task1 = asyncio.create_task(get_data(1))
+    # task1.add_done_callback(print_b)
+
+    # task1.result
+       
+
+
+    await asyncio.sleep(1)
+
+    # await task1
+    # await task2
+
+if __name__ == "__main__":
+    asyncio.run(main())
